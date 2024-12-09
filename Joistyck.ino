2b0d4779c3498aeb36a8f7da030e1b7bfac310aa
marilynLX <179334509+marilynLX@users.noreply.github.com>
2024-12-09 00:10:21 -0600

    Joistyck.ino

--- Joistyck.ino
+++ Joistyck.ino
@@ -1,7 +1,9 @@
-int y;
-  int x;
+//Declaramos variables
+int y; //para el eje y
+  int x; //para el eje x
   void setup (){
     Serial.begin(9600);
+    //declaramos como datos de salida
     pinMode(9,OUTPUT);
     pinMode(10,OUTPUT);
     pinMode(11,OUTPUT);
@@ -10,35 +12,38 @@ int y;
 joistic
 
 void loop() {
+  //leemos los valores del joystick 
  y = analogRead(A1);
  x = analogRead(A0);
+
+  //devolvemos los valores obtenidos al monitor
  Serial.print("y=");
  Serial.print(y);
  Serial.print("x=");
  Serial.println(x);
 delay(200);
 
-if (x>520){
+if (x>520){   // Si el joystick se mueve hacia la derecha enciende el pin 9
   digitalWrite(9,HIGH);
 }
 else 
-digitalWrite(9,LOW);
+digitalWrite(9,LOW); // y despues lo apaga
 
-if (x<500){
+if (x<500){  // Si el joystick se mueve hacia la izquierda enciende el pin 10
   digitalWrite(10,HIGH);
 }
 else 
-digitalWrite(10,LOW);
+digitalWrite(10,LOW);// y despues lo apaga
 
 if (y>1020){
-  digitalWrite(11,HIGH);
+  digitalWrite(11,HIGH);// Si el joystick se mueve hacia arriba se enciende el pin 11
 }
 else 
-digitalWrite(11,LOW);
+digitalWrite(11,LOW);// y despues lo apaga
 
-if (y<500){
+if (y<500){           // Si el joystick se mueve hacia abajo se enciende el pin 12
   digitalWrite(12,HIGH);
 }
 else 
-digitalWrite(12,LOW);
+digitalWrite(12,LOW);// y despues lo apaga
 }
