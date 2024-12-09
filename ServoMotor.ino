a84c16c8d386526f879d070f3450e3489f042963
marilynLX <179334509+marilynLX@users.noreply.github.com>
2024-12-09 00:27:21 -0600

    ServoMotor.ino

--- ServoMotor.ino
+++ ServoMotor.ino
@@ -1,31 +1,33 @@
+//descargamos una libreria para controlar el servo
 #include <Servo.h>
 Servo ioe;
-
+//declaramos variables
 int trig = 2;
 int echo = 3;
 int tiempo;
 int distancia;
 
 void setup () {
-  pinMode (trig, OUTPUT);
-  pinMode (echo, INPUT);
-  ioe.attach (4);
+  pinMode (trig, OUTPUT); //dato de salida
+  pinMode (echo, INPUT); //dato de entrada
+  ioe.attach (4); //asocia al servo al pin 4
 }
 
 void loop () {
-  digitalWrite (trig, HIGH);
-  delay (500);
-  digitalWrite (trig, LOW);
-  tiempo=pulseIn(echo, HIGH);
-  distancia=tiempo/58.2;
+   // Envío de un pulso al TRIG para activar el sensor ultrasónico
+  digitalWrite (trig, HIGH);// Genera un pulso de activación
+  delay (500);// Duración del pulso
+  digitalWrite (trig, LOW);  // Termina el pulso
+  tiempo=pulseIn(echo, HIGH);  // Lee el tiempo que tarda el pulso en regresar
+  distancia=tiempo/58.2;  // Calcula la distancia en centímetros
   delay(500);
   {
-    if (distancia <= 10)
-    (ioe.write(120));
+    if (distancia <= 10) // Controlamos el servomotor basandonos en la distancia medida y si la distancia es menor o igual a 10cm
+    (ioe.write(120)); //gira el servo a 120 grados
     delay(2000);
   }
   {
-    if (distancia >10);
-    ioe.write(45);
+    if (distancia >10);// Si la distancia es mayor a 10 cm
+    ioe.write(45);// Gira el servomotor a 45 grados
   }
 }
